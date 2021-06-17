e8f951be6e8f2585bcd7a3c74288690317452d9e
Drew Bradley <williamandrewb@gmail.com>
2021-06-17 09:55:06 -0600

    Add style to dropdown.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -41,11 +41,12 @@ const NavBar = ({notifications, deleteNotification}) => {
         size="lg"
         color="#B45AD3"/>
         {notifications.length}
-        <ul 
-          className="DropDown"
-          aria-label={`Notifications(${notifications.length})`}>
-          { toDropDown }
-        </ul>
+        <div className="DropDown">
+          <h2 className="drop-title">Notifications({notifications.length})</h2>
+          <ul className="droplist">
+            { toDropDown }
+          </ul>
+        </div>
       </div>
       
     </div>
